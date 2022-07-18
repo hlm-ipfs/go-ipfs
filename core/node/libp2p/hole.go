cc2589580942e3e682210dcf0687402890ef1e28
黄东 <shiliyuanma@gmail.com>
2022-07-18 17:23:35 +0800

    hole.log用ipfs默认的日志组建来记录

--- core/node/libp2p/hole.go
+++ core/node/libp2p/hole.go
@@ -1,29 +1,16 @@
 package libp2p
 
 import (
+	logging "github.com/ipfs/go-log/v2"
 	"github.com/libp2p/go-libp2p/p2p/protocol/holepunch"
-	microLogger "go-micro.dev/v4/logger"
-	"os"
-	"path/filepath"
-
-	"hlm-ipfs/x/infras"
-	"hlm-ipfs/x/logger"
 )
 
 func NewHoleTrace() *HoleTrace {
-	dir := "logs"
-	if str, ok := os.LookupEnv("IPFS_PATH"); ok && len(str) > 0 {
-		dir = filepath.Join(str, "log")
-		_ = os.Mkdir(dir, os.ModePerm)
-	}
-	l, err := logger.Create(logger.LoggerKindZap, logger.LoggerFormatterJson, microLogger.TraceLevel, dir, "hole", false)
-	infras.Throw(err)
-
-	return &HoleTrace{log: l}
+	return &HoleTrace{log: logging.Logger("hole")}
 }
 
 type HoleTrace struct {
-	log *logger.Logger
+	log *logging.ZapEventLogger
 }
 
 func (t *HoleTrace) Trace(evt *holepunch.Event) {
