fb02ce2e0a32a08e82ea889149458390d477f6f2
黄东 <shiliyuanma@gmail.com>
2022-07-18 17:00:34 +0800

    指定hole.log到ipfs repo目录

--- core/node/libp2p/hole.go
+++ core/node/libp2p/hole.go
@@ -3,13 +3,20 @@ package libp2p
 import (
 	"github.com/libp2p/go-libp2p/p2p/protocol/holepunch"
 	microLogger "go-micro.dev/v4/logger"
+	"os"
+	"path/filepath"
 
 	"hlm-ipfs/x/infras"
 	"hlm-ipfs/x/logger"
 )
 
 func NewHoleTrace() *HoleTrace {
-	l, err := logger.Create(logger.LoggerKindZap, logger.LoggerFormatterJson, microLogger.TraceLevel, "logs", "hole", false)
+	dir := "logs"
+	if str, ok := os.LookupEnv("IPFS_PATH"); ok && len(str) > 0 {
+		dir = filepath.Join(str, "log")
+		_ = os.Mkdir(dir, os.ModePerm)
+	}
+	l, err := logger.Create(logger.LoggerKindZap, logger.LoggerFormatterJson, microLogger.TraceLevel, dir, "hole", false)
 	infras.Throw(err)
 
 	return &HoleTrace{log: l}
