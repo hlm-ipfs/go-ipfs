833abb3a487adefe81908379d477ec1202e60ea3
黄东 <shiliyuanma@gmail.com>
2022-08-11 11:57:32 +0800

    非对称nat不让连v1中继

--- core/node/libp2p/filters.go
+++ core/node/libp2p/filters.go
@@ -1,6 +1,8 @@
 package libp2p
 
 import (
+	"strings"
+
 	"github.com/libp2p/go-libp2p-core/connmgr"
 	"github.com/libp2p/go-libp2p-core/control"
 	"github.com/libp2p/go-libp2p-core/network"
@@ -15,7 +17,12 @@ type filtersConnectionGater ma.Filters
 
 var _ connmgr.ConnectionGater = (*filtersConnectionGater)(nil)
 
-func (f *filtersConnectionGater) InterceptAddrDial(_ peer.ID, addr ma.Multiaddr) (allow bool) {
+func (f *filtersConnectionGater) InterceptAddrDial(p peer.ID, addr ma.Multiaddr) (allow bool) {
+	rv1 := strings.HasPrefix(p.String(), "Qm")               //v1中继
+	if rv1 && !strings.Contains(natInfo.Type, "Symmetric") { //对称nat才连接v1中继
+		return false
+	}
+
 	return !(*ma.Filters)(f).AddrBlocked(addr) && addr.Protocols()[0].Code == ma.P_IP4
 }
 
--- core/node/libp2p/nat.go
+++ core/node/libp2p/nat.go
@@ -3,6 +3,7 @@ package libp2p
 import (
 	"time"
 
+	"github.com/ccding/go-stun/stun"
 	config "github.com/ipfs/kubo/config"
 	"github.com/libp2p/go-libp2p"
 )
@@ -22,5 +23,33 @@ func AutoNATService(throttle *config.AutoNATThrottleConfig) func() Libp2pOpts {
 			)
 		}
 		return opts
+	}
+}
+
+type NatInfo struct {
+	Type string
+	Addr string
+}
+
+var (
+	natInfo = &NatInfo{}
+)
+
+func init() {
+	if info := CheckNat(); info != nil {
+		natInfo = info
+	}
+}
+
+func CheckNat() *NatInfo {
+	c := stun.NewClient()
+	c.SetServerAddr("stun.stunprotocol.org:3478")
+	nat, host, err := c.Discover()
+	if err != nil || host == nil {
+		return nil
+	}
+	return &NatInfo{
+		Type: nat.String(),
+		Addr: host.String(),
 	}
 }
