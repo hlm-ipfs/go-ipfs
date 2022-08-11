a90853da5bb3645f4eb87bedbf589983d90219ef
黄东 <shiliyuanma@gmail.com>
2022-08-11 16:43:15 +0800

    无法获取到nat类型的时候 也允许连接v1中继

--- core/node/libp2p/filters.go
+++ core/node/libp2p/filters.go
@@ -18,8 +18,8 @@ type filtersConnectionGater ma.Filters
 var _ connmgr.ConnectionGater = (*filtersConnectionGater)(nil)
 
 func (f *filtersConnectionGater) InterceptAddrDial(p peer.ID, addr ma.Multiaddr) (allow bool) {
-	rv1 := strings.HasPrefix(p.String(), "Qm")               //v1中继
-	if rv1 && !strings.Contains(natInfo.Type, "Symmetric") { //对称nat才连接v1中继
+	rv1 := strings.HasPrefix(p.String(), "Qm")                                        //v1中继
+	if rv1 && len(natInfo.Type) > 0 && !strings.Contains(natInfo.Type, "Symmetric") { //对称nat才连接v1中继
 		return false
 	}
 
